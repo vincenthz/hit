640adb2a43dd20f6432984d04a2b8531c64817c5
Vincent Hanquez <vincent@snarc.org>
2012-05-04 04:33:38 +0100

    add be16

--- Data/Git/Internal.hs
+++ Data/Git/Internal.hs
@@ -7,6 +7,7 @@
 --
 module Data.Git.Internal
         ( be32
+        , be16
         ) where
 
 import Data.Bits
@@ -18,3 +19,7 @@ be32 b = fromIntegral (B.index b 0) `shiftL` 24
        + fromIntegral (B.index b 1) `shiftL` 16
        + fromIntegral (B.index b 2) `shiftL` 8
        + fromIntegral (B.index b 3)
+
+be16 :: B.ByteString -> Word16
+be16 b = fromIntegral (B.index b 0) `shiftL` 8
+       + fromIntegral (B.index b 1)
