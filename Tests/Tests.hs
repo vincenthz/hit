d880a03ba342558ae29e08256a1bb1fff7af3fdd
Vincent Hanquez <vincent@snarc.org>
2012-09-09 08:19:43 +0100

    re-introduce tests for unmarshall.marshall==id

--- Tests/Tests.hs
+++ Tests/Tests.hs
@@ -16,12 +16,15 @@ import Data.Time.LocalTime
 import Data.Time.Clock
 import Data.Time.Calendar
 
+import Data.Maybe
+
 -- for arbitrary instance to generate only data that are writable
 -- to disk. i.e. no deltas.
---data ObjNoDelta = ObjNoDelta Object
+data ObjNoDelta = ObjNoDelta Object
+    deriving (Eq)
 
---instance Show ObjNoDelta where
---	show (ObjNoDelta o) = show o
+instance Show ObjNoDelta where
+    show (ObjNoDelta o) = show o
 
 arbitraryBS size = B.pack . map fromIntegral <$> replicateM size (choose (0,255) :: Gen Int)
 arbitraryBSno0 size = B.pack . map fromIntegral <$> replicateM size (choose (1,255) :: Gen Int)
@@ -30,24 +33,26 @@ arbitraryBSascii size = B.pack . map fromIntegral <$> replicateM size (choose (0
 arbitraryBSnoangle size = B.pack . map fromIntegral <$> replicateM size (choose (0x40,0x7f) :: Gen Int)
 
 instance Arbitrary Ref where
-	arbitrary = fromBinary <$> arbitraryBS 20
+    arbitrary = fromBinary <$> arbitraryBS 20
 
-arbitraryMsg = arbitraryBSno0 128
-arbitraryLazy = L.fromChunks . (:[]) <$> arbitraryBS 4096
+arbitraryMsg = arbitraryBSno0 1
+arbitraryLazy = L.fromChunks . (:[]) <$> arbitraryBS 40
 
 arbitraryRefList :: Gen [Ref]
 arbitraryRefList = replicateM 2 arbitrary
 
 arbitraryEnt = liftM3 (,,) arbitrary (arbitraryBSno0 48) arbitrary
-arbitraryEnts = choose (1,100) >>= \i -> replicateM i arbitraryEnt
+arbitraryEnts = choose (1,2) >>= \i -> replicateM i arbitraryEnt
 
 instance Arbitrary TimeZone where
-    arbitrary = hoursToTimeZone <$> arbitrary 
+    arbitrary = hoursToTimeZone . rel <$> arbitrary
+        where rel a = (a `mod` 24) - 12
 
 instance Arbitrary UTCTime where
     arbitrary = UTCTime <$> (flip addDays b <$> choose (0, 365 * 40))
                         <*> (secondsToDiffTime <$> arbitrary)
         where b = fromGregorian 1970 1 1
+
 arbitraryName = liftM4 (,,,) (arbitraryBSnoangle 16)
                              (arbitraryBSnoangle 16)
                              arbitrary
@@ -56,44 +61,42 @@ arbitraryName = liftM4 (,,,) (arbitraryBSnoangle 16)
 arbitraryObjTypeNoDelta = oneof [return TypeTree,return TypeBlob,return TypeCommit,return TypeTag]
 
 instance Arbitrary Commit where
-	arbitrary = Commit <$> arbitrary <*> arbitraryRefList <*> arbitraryName <*> arbitraryName <*> arbitraryMsg
+    arbitrary = Commit <$> arbitrary <*> arbitraryRefList <*> arbitraryName <*> arbitraryName <*> arbitraryMsg
 
 instance Arbitrary Tree where
-	arbitrary = Tree <$> arbitraryEnts
+    arbitrary = Tree <$> arbitraryEnts
 
 instance Arbitrary Blob where
-	arbitrary = Blob <$> arbitraryLazy
+    arbitrary = Blob <$> arbitraryLazy
 
 instance Arbitrary Tag where
-	arbitrary = Tag <$> arbitrary <*> arbitraryObjTypeNoDelta <*> arbitraryBSascii 20 <*> arbitraryName <*> arbitraryMsg
-
-{-
-instance Arbitrary Object where
-	arbitrary = undefined
+    arbitrary = Tag <$> arbitrary <*> arbitraryObjTypeNoDelta <*> arbitraryBSascii 20 <*> arbitraryName <*> arbitraryMsg
 
 instance Arbitrary ObjNoDelta where
-	arbitrary = ObjNoDelta <$> oneof
-		[ liftM5 Commit arbitrary arbitraryRefList arbitraryName arbitraryName arbitraryMsg
-		, liftM Tree arbitraryEnts
-		, liftM Blob arbitraryLazy
-		, liftM5 Tag
-		]
--}
+    arbitrary = ObjNoDelta <$> oneof
+        [ toObject <$> (arbitrary :: Gen Commit)
+        , toObject <$> (arbitrary :: Gen Tree)
+        , toObject <$> (arbitrary :: Gen Blob)
+        , toObject <$> (arbitrary :: Gen Tag)
+        ]
 
---prop_object_marshalling_id (ObjNoDelta obj) = obj == (looseUnmarshall $ looseMarshall obj)
+prop_object_marshalling_id (ObjNoDelta obj) = obj `assertEq` (looseUnmarshall $ looseMarshall obj)
+    where assertEq a b
+            | show a == show b    = True
+            | otherwise = error ("not equal:\n"  ++ show a ++ "\ngot: " ++ show b)
 
 refTests =
-	[ testProperty "hexadecimal" (marshEqual (fromHex . toHex))
-	, testProperty "binary" (marshEqual (fromBinary . toBinary))
-	]
-	where
-		marshEqual t ref = ref == t ref
+    [ testProperty "hexadecimal" (marshEqual (fromHex . toHex))
+    , testProperty "binary" (marshEqual (fromBinary . toBinary))
+    ]
+    where
+        marshEqual t ref = ref == t ref
 
 objTests =
-	[ -- testProperty "unmarshall.marshall==id" prop_object_marshalling_id
-	]
+    [ testProperty "unmarshall.marshall==id" prop_object_marshalling_id
+    ]
 
 main = defaultMain
-	[ testGroup "ref marshalling" refTests
-	, testGroup "object marshalling" objTests
-	]
+    [ testGroup "ref marshalling" refTests
+    , testGroup "object marshalling" objTests
+    ]
