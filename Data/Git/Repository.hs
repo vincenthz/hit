893e18cdd9edcbd9e4dbcb678697b8e2575c0411
Vincent Hanquez <vincent@snarc.org>
2013-04-27 08:13:44 +0100

    reindent module a bit better

--- Data/Git/Repository.hs
+++ Data/Git/Repository.hs
@@ -68,7 +68,7 @@ getCommitMaybe git ref = maybe Nothing objectToCommit <$> getObject git ref True
 -- | get a specified commit but raises an exception if doesn't exists or type is not appropriate
 getCommit :: Git -> Ref -> IO Commit
 getCommit git ref = maybe err id . objectToCommit <$> getObject_ git ref True
-    where err = throw $ InvalidType ref TypeCommit
+  where err = throw $ InvalidType ref TypeCommit
 
 -- | get a specified tree
 getTreeMaybe :: Git -> Ref -> IO (Maybe Tree)
@@ -77,75 +77,75 @@ getTreeMaybe git ref = maybe Nothing objectToTree <$> getObject git ref True
 -- | get a specified tree but raise
 getTree :: Git -> Ref -> IO Tree
 getTree git ref = maybe err id . objectToTree <$> getObject_ git ref True
-    where err = throw $ InvalidType ref TypeTree
+  where err = throw $ InvalidType ref TypeTree
 
 -- | try to resolve a string to a specific commit ref
 -- for example: HEAD, HEAD^, master~3, shortRef
 resolveRevision :: Git -> Revision -> IO (Maybe Ref)
 resolveRevision git (Revision prefix modifiers) =
     getCacheVal (packedNamed git) >>= \c -> resolvePrefix c >>= modf modifiers
-    where
-          resolvePrefix lookupCache = tryResolvers
-                [resolveNamedPrefix lookupCache namedResolvers
-                ,resolvePrePrefix
-                ]
+  where
+        resolvePrefix lookupCache = tryResolvers
+              [resolveNamedPrefix lookupCache namedResolvers
+              ,resolvePrePrefix
+              ]
 
-          resolveNamedPrefix _           []     = return Nothing
-          resolveNamedPrefix lookupCache (x:xs) = followToRef (resolveNamedPrefix lookupCache xs) x
-            where followToRef onFailure refty = do
-                      exists <- existsRefFile (gitRepoPath git) refty
-                      if exists
-                          then do refcont <- readRefFile (gitRepoPath git) refty
-                                  case refcont of
-                                       RefDirect ref     -> return $ Just ref
-                                       RefLink refspecty -> followToRef onFailure refspecty
-                                       _                 -> error "cannot handle reference content"
-                          else case M.lookup refty lookupCache of
-                                    Nothing -> onFailure
-                                    y       -> return y
+        resolveNamedPrefix _           []     = return Nothing
+        resolveNamedPrefix lookupCache (x:xs) = followToRef (resolveNamedPrefix lookupCache xs) x
+          where followToRef onFailure refty = do
+                    exists <- existsRefFile (gitRepoPath git) refty
+                    if exists
+                        then do refcont <- readRefFile (gitRepoPath git) refty
+                                case refcont of
+                                     RefDirect ref     -> return $ Just ref
+                                     RefLink refspecty -> followToRef onFailure refspecty
+                                     _                 -> error "cannot handle reference content"
+                        else case M.lookup refty lookupCache of
+                                  Nothing -> onFailure
+                                  y       -> return y
 
-          namedResolvers = case prefix of
-                               "HEAD"       -> [ RefHead ]
-                               "ORIG_HEAD"  -> [ RefOrigHead ]
-                               "FETCH_HEAD" -> [ RefFetchHead ]
-                               _            -> [ RefTag prefix, RefBranch prefix, RefRemote prefix ]
+        namedResolvers = case prefix of
+                             "HEAD"       -> [ RefHead ]
+                             "ORIG_HEAD"  -> [ RefOrigHead ]
+                             "FETCH_HEAD" -> [ RefFetchHead ]
+                             _            -> [ RefTag prefix, RefBranch prefix, RefRemote prefix ]
 
 
-          tryResolvers :: [IO (Maybe Ref)] -> IO Ref
-          tryResolvers []            = return $ fromHexString prefix
-          tryResolvers (resolver:xs) = resolver >>= isResolved
-             where isResolved (Just r) = return r
-                   isResolved Nothing  = tryResolvers xs
+        tryResolvers :: [IO (Maybe Ref)] -> IO Ref
+        tryResolvers []            = return $ fromHexString prefix
+        tryResolvers (resolver:xs) = resolver >>= isResolved
+           where isResolved (Just r) = return r
+                 isResolved Nothing  = tryResolvers xs
 
-          resolvePrePrefix :: IO (Maybe Ref)
-          resolvePrePrefix = do
-              refs <- findReferencesWithPrefix git prefix
-              case refs of
-                  []  -> return Nothing
-                  [r] -> return (Just r)
-                  _   -> error "multiple references with this prefix"
+        resolvePrePrefix :: IO (Maybe Ref)
+        resolvePrePrefix = do
+            refs <- findReferencesWithPrefix git prefix
+            case refs of
+                []  -> return Nothing
+                [r] -> return (Just r)
+                _   -> error "multiple references with this prefix"
 
-          modf [] ref                  = return (Just ref)
-          modf (RevModParent i:xs) ref = do
-              parentRefs <- getParentRefs ref
-              case i of
-                  0 -> error "revision modifier ^0 is not implemented"
-                  _ -> case drop (i - 1) parentRefs of
-                            []    -> error "no such parent"
-                            (p:_) -> modf xs p
+        modf [] ref                  = return (Just ref)
+        modf (RevModParent i:xs) ref = do
+            parentRefs <- getParentRefs ref
+            case i of
+                0 -> error "revision modifier ^0 is not implemented"
+                _ -> case drop (i - 1) parentRefs of
+                          []    -> error "no such parent"
+                          (p:_) -> modf xs p
 
-          modf (RevModParentFirstN 1:xs) ref = modf (RevModParent 1:xs) ref
-          modf (RevModParentFirstN n:xs) ref = do
-              parentRefs <- getParentRefs ref
-              modf (RevModParentFirstN (n-1):xs) (head parentRefs)
-          modf (_:_) _ = error "unimplemented revision modifier"
+        modf (RevModParentFirstN 1:xs) ref = modf (RevModParent 1:xs) ref
+        modf (RevModParentFirstN n:xs) ref = do
+            parentRefs <- getParentRefs ref
+            modf (RevModParentFirstN (n-1):xs) (head parentRefs)
+        modf (_:_) _ = error "unimplemented revision modifier"
 
-          getParentRefs ref = commitParents <$> getCommit git ref
+        getParentRefs ref = commitParents <$> getCommit git ref
 
 -- | returns a tree from a ref that might be either a commit, a tree or a tag.
 resolveTreeish :: Git -> Ref -> IO (Maybe Tree)
-resolveTreeish git ref = getObject git ref True >>= mapJustM recToTree where
-        recToTree (objectToCommit -> Just (Commit { commitTreeish = tree })) = resolveTreeish git tree
+resolveTreeish git ref = getObject git ref True >>= mapJustM recToTree
+  where recToTree (objectToCommit -> Just (Commit { commitTreeish = tree })) = resolveTreeish git tree
         recToTree (objectToTag    -> Just (Tag tref _ _ _ _))    = resolveTreeish git tref
         recToTree (objectToTree   -> Just t@(Tree _))            = return $ Just t
         recToTree _                                              = return Nothing
@@ -173,35 +173,35 @@ rewrite git mapCommit revision nbParent = do
     ref <- fromMaybe (error "revision cannot be found") <$> resolveRevision git revision
     resolveParents nbParent ref >>= process . reverse
 
-    where resolveParents :: Int -> Ref -> IO [ (Ref, Commit) ]
-          resolveParents 0 ref = (:[]) . (,) ref <$> getCommit git ref
-          resolveParents n ref = do commit <- getCommit git ref
-                                    case commitParents commit of
-                                         [parentRef] -> liftM ((ref,commit) :) (resolveParents (n-1) parentRef)
-                                         _           -> return [(ref,commit)]
+  where resolveParents :: Int -> Ref -> IO [ (Ref, Commit) ]
+        resolveParents 0 ref = (:[]) . (,) ref <$> getCommit git ref
+        resolveParents n ref = do commit <- getCommit git ref
+                                  case commitParents commit of
+                                       [parentRef] -> liftM ((ref,commit) :) (resolveParents (n-1) parentRef)
+                                       _           -> return [(ref,commit)]
 
-          process [] = error "nothing to rewrite"
-          process ((_,commit):next) =
-                      mapCommit commit >>= looseWrite (gitRepoPath git) . toObject >>= flip rewriteOne next
+        process [] = error "nothing to rewrite"
+        process ((_,commit):next) =
+                    mapCommit commit >>= looseWrite (gitRepoPath git) . toObject >>= flip rewriteOne next
 
-          rewriteOne prevRef [] = return prevRef
-          rewriteOne prevRef ((_,commit):next) = do
-                      newCommit <- mapCommit $ commit { commitParents = [prevRef] }
-                      ref       <- looseWrite (gitRepoPath git) (toObject newCommit)
-                      rewriteOne ref next
+        rewriteOne prevRef [] = return prevRef
+        rewriteOne prevRef ((_,commit):next) = do
+                    newCommit <- mapCommit $ commit { commitParents = [prevRef] }
+                    ref       <- looseWrite (gitRepoPath git) (toObject newCommit)
+                    rewriteOne ref next
 
 -- | build a hierarchy tree from a tree object
 buildHTree :: Git -> Tree -> IO HTree
 buildHTree git (Tree ents) = mapM resolveTree ents
-        where resolveTree (perm, ent, ref) = do
-                obj <- getObjectType git ref
-                case obj of
-                        Just TypeBlob -> return (perm, ent, TreeFile ref)
-                        Just TypeTree -> do ctree <- getTree git ref
-                                            dir   <- buildHTree git ctree
-                                            return (perm, ent, TreeDir ref dir)
-                        Just _        -> error "wrong type embedded in tree object"
-                        Nothing       -> error "unknown reference in tree object"
+  where resolveTree (perm, ent, ref) = do
+            obj <- getObjectType git ref
+            case obj of
+                Just TypeBlob -> return (perm, ent, TreeFile ref)
+                Just TypeTree -> do ctree <- getTree git ref
+                                    dir   <- buildHTree git ctree
+                                    return (perm, ent, TreeDir ref dir)
+                Just _        -> error "wrong type embedded in tree object"
+                Nothing       -> error "unknown reference in tree object"
 
 -- | resolve the ref (tree or blob) related to a path at a specific commit ref
 resolvePath :: Git            -- ^ repository
@@ -209,16 +209,15 @@ resolvePath :: Git            -- ^ repository
             -> [ByteString]   -- ^ paths
             -> IO (Maybe Ref)
 resolvePath git commitRef paths =
-        getCommit git commitRef >>= \commit -> resolve (commitTreeish commit) paths
-    where
-                resolve :: Ref -> [ByteString] -> IO (Maybe Ref)
-                resolve treeRef []     = return $ Just treeRef
-                resolve treeRef (x:xs) = do
-                        (Tree ents) <- getTree git treeRef
-                        let cEnt = treeEntRef <$> findEnt x ents
-                        if xs == []
-                            then return cEnt
-                            else maybe (return Nothing) (\z -> resolve z xs) cEnt
+    getCommit git commitRef >>= \commit -> resolve (commitTreeish commit) paths
+  where resolve :: Ref -> [ByteString] -> IO (Maybe Ref)
+        resolve treeRef []     = return $ Just treeRef
+        resolve treeRef (x:xs) = do
+            (Tree ents) <- getTree git treeRef
+            let cEnt = treeEntRef <$> findEnt x ents
+            if xs == []
+                then return cEnt
+                else maybe (return Nothing) (\z -> resolve z xs) cEnt
 
-                findEnt x = find (\(_, b, _) -> b == x)
-                treeEntRef (_,_,r) = r
+        findEnt x = find (\(_, b, _) -> b == x)
+        treeEntRef (_,_,r) = r
